531ac53beffddd007b43fa4c7c978c1511b3512e
Tim Docker <tim@dockerz.net>
2021-08-05 09:18:11 +1000

    switch to stm32f401 with HSE 25Mhz

--- firmware/memory.x
+++ firmware/memory.x
@@ -1,6 +1,12 @@
 /* Linker script for the STM32F103C8T6 */
 MEMORY
 {
-  FLASH : ORIGIN = 0x08000000, LENGTH = 64K
-  RAM : ORIGIN = 0x20000000, LENGTH = 20K
+  FLASH : ORIGIN = 0x08000000, LENGTH = 512K
+  RAM : ORIGIN = 0x20000000, LENGTH = 96K
 }
+
+
+/* This is where the call stack will be allocated. */
+/* The stack is of the full descending type. */
+/* NOTE Do NOT modify `_stack_start` unless you know what you are doing */
+_stack_start = ORIGIN(RAM) + LENGTH(RAM);
--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -4,13 +4,11 @@
 use rtt_target::{rtt_init_print, rprintln};
 
 // set the panic handler
-extern crate panic_halt;
+extern crate panic_rtt_target;
 
-use cortex_m_rt::{entry};
-use stm32f1xx_hal::{prelude::*, stm32, spi, delay::Delay};
+use stm32f4xx_hal::{prelude::*, spi, delay::Delay};
 
 use rtic::app;
-use rtic::cyccnt::{Duration};
 
 use ili9341::{Ili9341, Orientation};
 use embedded_graphics::{
@@ -21,61 +19,58 @@ use embedded_graphics::{
 };
 use display_interface_spi::SPIInterface;
 
-type Display = ili9341::Ili9341<
- display_interface_spi::SPIInterface<stm32f1xx_hal::spi::Spi<stm32f1xx_hal::pac::SPI1, stm32f1xx_hal::spi::Spi1NoRemap, (stm32f1xx_hal::gpio::gpioa::PA5<stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::PushPull>>, stm32f1xx_hal::gpio::gpioa::PA6<stm32f1xx_hal::gpio::Input<stm32f1xx_hal::gpio::Floating>>, stm32f1xx_hal::gpio::gpioa::PA7<stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::PushPull>>), u8>, stm32f1xx_hal::gpio::gpioa::PA3<stm32f1xx_hal::gpio::Output<stm32f1xx_hal::gpio::PushPull>>, stm32f1xx_hal::gpio::gpioa::PA2<stm32f1xx_hal::gpio::Output<stm32f1xx_hal::gpio::PushPull>>>,
- stm32f1xx_hal::gpio::gpioa::PA4<stm32f1xx_hal::gpio::Output<stm32f1xx_hal::gpio::PushPull>>
- >;
+type Display = ili9341::Ili9341<display_interface_spi::SPIInterface<stm32f4xx_hal::spi::Spi<stm32f4xx_hal::stm32::SPI1, (stm32f4xx_hal::gpio::gpioa::PA5<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::AF5>>, stm32f4xx_hal::gpio::gpioa::PA6<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::AF5>>, stm32f4xx_hal::gpio::gpioa::PA7<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::AF5>>)>, stm32f4xx_hal::gpio::gpioa::PA3<stm32f4xx_hal::gpio::Output<stm32f4xx_hal::gpio::PushPull>>, stm32f4xx_hal::gpio::gpioa::PA2<stm32f4xx_hal::gpio::Output<stm32f4xx_hal::gpio::PushPull>>>, stm32f4xx_hal::gpio::gpioa::PA4<stm32f4xx_hal::gpio::Output<stm32f4xx_hal::gpio::PushPull>>>;
 
-#[app(device = stm32f1xx_hal::pac, peripherals = true)]
+#[app(device = stm32f4xx_hal::pac, peripherals = true)]
 const APP: () = {
   struct Resources {
     display: Display
   }
 
   #[init(schedule = [])]
-  fn init(mut cx: init::Context) -> init::LateResources {
+  fn init(cx: init::Context) -> init::LateResources {
     rtt_init_print!();
     rprintln!("Starting init");
     rprintln!("Starting init2");
 
-    cx.core.DCB.enable_trace();
+    //cx.core.DCB.enable_trace();
     // required on Cortex-M7 devices that software lock the DWT (e.g. STM32F7)
-    cortex_m::peripheral::DWT::unlock();
-    cx.core.DWT.enable_cycle_counter();
+    //cortex_m::peripheral::DWT::unlock();
+    //cx.core.DWT.enable_cycle_counter();
+
+    rprintln!("Starting init2.5");
+
 
     // Take ownership over the raw flash and rcc devices and convert them into the corresponding
     // HAL structs
-    let mut flash = cx.device.FLASH.constrain();
-    let mut rcc = cx.device.RCC.constrain();
+    let rcc = cx.device.RCC.constrain();
 
     let clocks = rcc
       .cfgr
-      .use_hse(8.mhz())
+      .use_hse(25.mhz())
       .sysclk(32.mhz())
-      .freeze(&mut flash.acr);
+      .freeze();
 
-    let mut afio = cx.device.AFIO.constrain(&mut rcc.apb2);
+    // let mut afio = cx.device.AFIO.constrain();
     rprintln!("Starting init3");
 
 
     // configure the gpio ports
-    let mut gpioa = cx.device.GPIOA.split(&mut rcc.apb2);
+    let gpioa = cx.device.GPIOA.split();
 
     // configure the display driver
-    let cs    = gpioa.pa2.into_push_pull_output(&mut gpioa.crl);
-    let dc    = gpioa.pa3.into_push_pull_output(&mut gpioa.crl);
-    let reset = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
-    let sck   = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
-    let miso  = gpioa.pa6;
-    let mosi  = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+    let cs    = gpioa.pa2.into_push_pull_output();
+    let dc    = gpioa.pa3.into_push_pull_output();
+    let reset = gpioa.pa4.into_push_pull_output();
+    let sck   = gpioa.pa5.into_alternate_af5();
+    let miso  = gpioa.pa6.into_alternate_af5();
+    let mosi  = gpioa.pa7.into_alternate_af5();
     let spi   = spi::Spi::spi1(
         cx.device.SPI1,
         (sck, miso, mosi),
-        &mut afio.mapr,
         ili9341::SPI_MODE,
-        16.mhz(),
+        16_000_000.hz(),
         clocks,
-        &mut rcc.apb2,
     );
 
     let mut delay = Delay::new(cx.core.SYST, clocks);
@@ -96,10 +91,11 @@ const APP: () = {
 
     rprintln!("Starting init5");
 
+
     Text::with_text_style(test_text, Point::zero(), character_style, text_style)
         .draw(&mut display).unwrap();
 
-    rprintln!("Starting init6");
+        rprintln!("Starting init5");
 
     init::LateResources {
       display,
