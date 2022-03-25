e29c795925632c68b9b53720af99181fe01f97a7
Tim Docker <tim@dockerz.net>
2022-03-25 17:30:28 +1100

    Read Vbat in a rtic background task

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -23,6 +23,8 @@ use stm32f4xx_hal::{
   serial
 };
 
+use dwt_systick_monotonic::DwtSystick;
+
 use rtt_target::{rtt_init_print, rprintln};
 
 use nb::block;
@@ -33,16 +35,22 @@ use crate::layout;
 use crate::debouncer;
 use crate::memory_display;
 
-type Display = crate::memory_display::MemoryDisplay<stm32f4xx_hal::spi::Spi<stm32f4xx_hal::pac::SPI1, (stm32f4xx_hal::gpio::Pin<stm32f4xx_hal::gpio::Input<stm32f4xx_hal::gpio::Floating>, 'A', 5_u8>, stm32f4xx_hal::gpio::NoPin, stm32f4xx_hal::gpio::Pin<stm32f4xx_hal::gpio::Input<stm32f4xx_hal::gpio::Floating>, 'A', 7_u8>), stm32f4xx_hal::spi::TransferModeNormal>, stm32f4xx_hal::gpio::Pin<stm32f4xx_hal::gpio::Output<stm32f4xx_hal::gpio::PushPull>, 'A', 4_u8>, stm32f4xx_hal::timer::SysDelay, 12000_usize, 240_usize>;
+type Display = crate::memory_display::MemoryDisplay<stm32f4xx_hal::spi::Spi<stm32f4xx_hal::pac::SPI1, (stm32f4xx_hal::gpio::Pin<stm32f4xx_hal::gpio::Input<stm32f4xx_hal::gpio::Floating>, 'A', 5_u8>, stm32f4xx_hal::gpio::NoPin, stm32f4xx_hal::gpio::Pin<stm32f4xx_hal::gpio::Input<stm32f4xx_hal::gpio::Floating>, 'A', 7_u8>), stm32f4xx_hal::spi::TransferModeNormal>, stm32f4xx_hal::gpio::Pin<stm32f4xx_hal::gpio::Output<stm32f4xx_hal::gpio::PushPull>, 'A', 4_u8>, stm32f4xx_hal::timer::Delay<stm32f4xx_hal::pac::TIM5, 1000000_u32>, 12000_usize, 240_usize>;
 type Serial = stm32f4xx_hal::serial::Serial<stm32f4xx_hal::pac::USART1, (stm32f4xx_hal::gpio::Pin<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::PushPull, 7_u8>, 'A', 9_u8>, stm32f4xx_hal::gpio::Pin<stm32f4xx_hal::gpio::Alternate<stm32f4xx_hal::gpio::PushPull, 7_u8>, 'A', 10_u8>), u8>;
 type Key = stm32f4xx_hal::gpio::gpioa::PA0<stm32f4xx_hal::gpio::Input<stm32f4xx_hal::gpio::PullUp>>;
 type Adc = stm32f4xx_hal::adc::Adc<stm32f4xx_hal::pac::ADC1>;
 type Vin = stm32f4xx_hal::gpio::gpioa::PA1<stm32f4xx_hal::gpio::Analog>;
+type Led = stm32f4xx_hal::gpio::gpioc::PC13<stm32f4xx_hal::gpio::Output<stm32f4xx_hal::gpio::PushPull>>;
 
+const MONO_HZ: u32 = 84_000_000; // 8 MHz
+
+#[monotonic(binds = SysTick, default = true)]
+type MyMono = DwtSystick<MONO_HZ>;
 
   #[shared]
   struct Shared {
     gps: Gps,
+    vbat_mv: Option<u16>,
   }
 
   #[local]
@@ -51,7 +59,8 @@ type Vin = stm32f4xx_hal::gpio::gpioa::PA1<stm32f4xx_hal::gpio::Analog>;
     display: Display,
     key: Key,
     adc: Adc,
-    vin: Vin,
+    vbatin: Vin,
+    led: Led,
   }
 
   #[init()]
@@ -69,6 +78,11 @@ type Vin = stm32f4xx_hal::gpio::gpioa::PA1<stm32f4xx_hal::gpio::Analog>;
       .sysclk(32.MHz())
       .freeze();
 
+    let mut dcb = cx.core.DCB;
+    let dwt = cx.core.DWT;
+    let systick = cx.core.SYST;
+    let mono = DwtSystick::new(&mut dcb, dwt, systick, MONO_HZ);
+
     // Enable debugging in sleep modes so that stlink stays alive during wfi etc._
     // Remove this if/when power consumption is an issue.
     cx.device.DBGMCU.cr.write( |w| w
@@ -79,8 +93,10 @@ type Vin = stm32f4xx_hal::gpio::gpioa::PA1<stm32f4xx_hal::gpio::Analog>;
 
     // configure the gpio ports
     let gpioa = cx.device.GPIOA.split();
+    let gpioc = cx.device.GPIOC.split();
     let key   = gpioa.pa0.into_pull_up_input();
-    let vin = gpioa.pa1.into_analog();
+    let vbatin   = gpioa.pa1.into_analog();
+    let led   = gpioc.pc13.into_push_pull_output();
 
     // configure the display driver
     let cs = gpioa.pa4.into_push_pull_output();
@@ -94,7 +110,7 @@ type Vin = stm32f4xx_hal::gpio::gpioa::PA1<stm32f4xx_hal::gpio::Analog>;
         &clocks,
     );
 
-    let delay = cx.core.SYST.delay(&clocks);
+    let delay = cx.device.TIM5.delay_us(&clocks);
     let display = memory_display::new_ls027b7dh01(spi, cs, delay);
 
     // Configure the serial port for GPS data
@@ -112,8 +128,12 @@ type Vin = stm32f4xx_hal::gpio::gpioa::PA1<stm32f4xx_hal::gpio::Analog>;
     let adc_config = adc::config::AdcConfig::default();
     let adc = adc::Adc::adc1(cx.device.ADC1, true, adc_config);
 
+    // Periodically read the battery voltage
+    read_batv::spawn_after(250.millis()).unwrap();
+
     let shared = Shared {
       gps: Gps::new(),
+      vbat_mv: Some(0),
     };
 
     let local = Local {
@@ -121,10 +141,11 @@ type Vin = stm32f4xx_hal::gpio::gpioa::PA1<stm32f4xx_hal::gpio::Analog>;
       display,
       key,
       adc,
-      vin,
+      vbatin,
+      led,
     };
 
-    (shared, local, init::Monotonics())
+    (shared, local, init::Monotonics(mono))
 
   }
 
@@ -142,12 +163,14 @@ type Vin = stm32f4xx_hal::gpio::gpioa::PA1<stm32f4xx_hal::gpio::Analog>;
     }
   }
 
-  #[idle(shared=[gps], local=[key,display,adc,vin])]
+  #[idle(shared=[gps, vbat_mv], local=[key,display])]
   fn idle(mut cx: idle::Context) -> ! {
     let mut screens = layout::Screens::new();
     let mut debounce = debouncer::Debouncer::new(3);
 
     screens.render_initial(cx.local.display).unwrap();
+    screens.update_vbat(0);
+
     cx.local.display.refresh();
 
     loop {
@@ -181,21 +204,33 @@ type Vin = stm32f4xx_hal::gpio::gpioa::PA1<stm32f4xx_hal::gpio::Analog>;
 
       if let Some(vtg) = ovtg {
         screens.update_vtg(vtg, &speed_stats);
-        
         updated = true;
       }
 
-      
-      let sample = cx.local.adc.convert(cx.local.vin, adc::config::SampleTime::Cycles_480);
-      let vbat = cx.local.adc.sample_to_millivolts(sample) * 2;
-      screens.update_vbat(vbat);
-      updated = true;
+      let ovbat_mv = cx.shared.vbat_mv.lock( |vbat_mv| {vbat_mv.take()} );
+      if let Some(vbat_mv) = ovbat_mv {
+        screens.update_vbat(vbat_mv);
+        updated = true;
+      }
 
       if updated {
         screens.render_update(cx.local.display).unwrap();
       }
       cx.local.display.refresh();
+
     }
+  }
+
+  #[task(local=[adc,vbatin,led], shared=[vbat_mv])]
+  fn read_batv(mut cx:  read_batv::Context) {
+
+    // Read the pin voltage, and write it to the shared variable
+    let sample = cx.local.adc.convert(cx.local.vbatin, adc::config::SampleTime::Cycles_480);
+    let mv = cx.local.adc.sample_to_millivolts(sample) * 2;
+    cx.shared.vbat_mv.lock( |vbat_mv| *vbat_mv = Some(mv) );
+
+    cx.local.led.toggle();
+    read_batv::spawn_after(250.millis()).unwrap();
   }
 }
 
